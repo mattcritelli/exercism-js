50fb4484cbe2d41502ab052bbb24f7bc5f2dfbbf
Matt C <matthew.critelli@gmail.com>
2019-11-13 10:25:37 -0700

    Update second solution with memoization.

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -1,18 +1,24 @@
 export class Matrix {
   constructor(data) {
-    this.data = data
+    this.data = data;
   }
 
   get rows() {
-    return this.data
-      .split('\n')
-      .map(row => row
-        .split(' ')
-        .map(strNum => Number(strNum)))
+    if (this._rows_) return this._rows_;
+
+    this._rows_ = this.data
+        .split('\n')
+        .map(row => row
+          .split(' ')
+          .map(strNum => Number(strNum)));
+
+    return this._rows_;
   }
 
   get columns() {
-    let output = [];
+    if (this._columns_) return this._columns_;
+
+    this._columns_ = [];
 
     let rows = this.data
       .split('\n')
@@ -21,14 +27,13 @@ export class Matrix {
         .map(strNum => Number(strNum)));
 
     for(let i = 0; i < rows.length; i++) {
-
       for(let j = 0; j < rows[i].length; j++) {
-        if(!output[j]) {
-          output[j] = [];
+        if (!this._columns_[j]) {
+          this._columns_[j] = [];
         }
-        output[j].push(Number(rows[i][j]));
+        this._columns_[j].push(Number(rows[i][j]));
       }
     }
-    return output;
+    return this._columns_;
   }
 }
