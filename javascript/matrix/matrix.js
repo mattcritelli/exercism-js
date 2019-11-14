c394fe551785e263a4a7011048d2637f81405fe2
Matt C <matthew.critelli@gmail.com>
2019-11-14 10:57:03 -0700

    Refactor row logic to separate helper function and lazy loading.

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -3,37 +3,34 @@ export class Matrix {
     this.data = data;
   }
 
-  get rows() {
-    if (this._rows_) return this._rows_;
+  generateRows(data) {
+    return data.split('\n')
+      .map(row => row
+        .split(' ')
+        .map(strNum => Number(strNum))
+      );
+  }
 
-    this._rows_ = this.data
-        .split('\n')
-        .map(row => row
-          .split(' ')
-          .map(strNum => Number(strNum)));
+  get rows() {
+    if (!this._rows_) this._rows_ = this.generateRows(this.data);
 
     return this._rows_;
   }
 
   get columns() {
-    if (this._columns_) return this._columns_;
+    // Not an issue with our test cases but added this statement in case user
+    // ran 'get columns' first since it is dependent upon this._rows_ existing
+    if (!this._rows) this._rows_ = this.generateRows(this.data);
+    if (!this._columns_) this._columns_ = [];
 
-    this._columns_ = [];
+    this._rows_
+      .forEach((row, i) =>
+        row.forEach((num, j) => {
+          if(!this._columns_[j]) this._columns_[j] = [];
+          this._columns_[j][i] = num;
+        })
+    );
 
-    let rows = this.data
-      .split('\n')
-      .map(row => row
-        .split(' ')
-        .map(strNum => Number(strNum)));
-
-    for(let i = 0; i < rows.length; i++) {
-      for(let j = 0; j < rows[i].length; j++) {
-        if (!this._columns_[j]) {
-          this._columns_[j] = [];
-        }
-        this._columns_[j].push(Number(rows[i][j]));
-      }
-    }
     return this._columns_;
   }
 }
